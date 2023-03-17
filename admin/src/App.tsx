a95d026230c96214b3244f12f45949cd73de947c
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-03-18 00:39:39 +0200

    Fix all

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,39 +1,46 @@
-import {Admin, DataProvider, Loading, Resource} from 'react-admin'
-import {CssBaseline} from '@mui/material'
-import {useEffect, useState} from "react";
-import buildHasuraProvider from 'ra-data-hasura'
-import {MenuList} from "./modules/menu/components/menu-list/menu-list.component";
-import {MenuEdit} from "./modules/menu/components/menu-edit/menu-edit.component";
-import {MenuCreate} from "./modules/menu/components/menu-create/menu-create.component";
+import { Admin, DataProvider, Loading, Resource } from 'react-admin';
+import { CssBaseline } from '@mui/material';
+import { useEffect, useState } from 'react';
+import buildHasuraProvider from 'ra-data-hasura';
+import { MenuList } from './modules/menu/components/menu-list/menu-list.component';
+import { MenuEdit } from './modules/menu/components/menu-edit/menu-edit.component';
+import { MenuCreate } from './modules/menu/components/menu-create/menu-create.component';
 
 export const App = () => {
-    const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(null)
-    useEffect(() => {
-        const buildDataProvider = async () => {
-            const dp = await buildHasuraProvider({
-                clientOptions: {
-                    uri: 'http://localhost:8080/v1/graphql',
-                },
-            });
-            setDataProvider(dp)
-        }
-        buildDataProvider()
-    },[])
+  const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(
+    null
+  );
+  useEffect(() => {
+    const buildDataProvider = async () => {
+      const dp = await buildHasuraProvider({
+        clientOptions: {
+          uri: 'http://localhost:8080/v1/graphql',
+        },
+      });
 
-    if(!dataProvider){
-        return <Loading/>
-    }
-    return (
-        <>
-            <CssBaseline/>
-            <Admin dataProvider={dataProvider}>
-                <Resource name='menu'
-                          list={MenuList}
-                          edit={MenuEdit}
-                          create={MenuCreate}/>
-            </Admin>
-        </>
-    )
-}
+      setDataProvider(dp);
+    };
+
+    buildDataProvider();
+  }, []);
+
+  if (!dataProvider) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <CssBaseline />
+      <Admin dataProvider={dataProvider}>
+        <Resource
+          name="menu"
+          list={MenuList}
+          edit={MenuEdit}
+          create={MenuCreate}
+        />
+      </Admin>
+    </>
+  );
+};
 
 export default App;
