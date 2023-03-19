85530c2b05689517653d659ccacd3688dd37c4ba
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-03-19 22:23:14 +0200

    Add graphql-tag loader

---
+++ admin/craco.webpack.js
@@ -0,0 +1,12 @@
+module.exports = {
+    overrideWebpackConfig:({webpackConfig}) => {
+        webpackConfig.module.rules.push(
+            {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: 'graphql-tag/loader',
+            },
+        );
+        return webpackConfig
+    }
+}
